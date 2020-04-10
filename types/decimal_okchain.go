f3e8b0b6ed23975d62704d501c68ece87e0892d6
Zhong Qiu <36867992+zhongqiuwood@users.noreply.github.com>
2020-04-10 08:00:23 +0800

    Delete decimal_okchain.go

--- types/decimal_okchain.go
+++
@@ -1,9 +0,0 @@
-package types
-
-import "math/big"
-
-// Round a decimal with precision, perform bankers rounding (gaussian rounding)
-func (d Dec) RoundDecimal(precision int64) Dec {
-	precisionMul := NewIntFromBigInt(new(big.Int).Exp(big.NewInt(10), big.NewInt(precision), nil))
-	return newDecFromInt(d.MulInt(precisionMul).RoundInt()).QuoInt(precisionMul)
-}
