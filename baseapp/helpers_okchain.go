f6c27dac4c0ef4f1b92162b5248d5dcba6d2965e
Qiu Zhong <zhongqiuwood@gmail.com>
2020-11-14 12:35:35 +0800

    rm func (app *BaseApp) GetDeliverStateCtx() sdk.Context

--- baseapp/helpers_okchain.go
+++ baseapp/helpers_okchain.go
@@ -4,34 +4,6 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
-
-func (app *BaseApp) GetCommitMultiStore() sdk.CommitMultiStore {
-	return app.cms
-}
-
-func (app *BaseApp) GetDeliverStateCtx() sdk.Context {
-	return app.deliverState.ctx
-}
-
-//-------------------------------------------------------
-// for protocol engine to invoke
-//-------------------------------------------------------
-func (app *BaseApp) PushInitChainer(initChainer sdk.InitChainer) {
-	app.initChainer = initChainer
-}
-
-func (app *BaseApp) PushBeginBlocker(beginBlocker sdk.BeginBlocker) {
-	app.beginBlocker = beginBlocker
-}
-
-func (app *BaseApp) PushEndBlocker(endBlocker sdk.EndBlocker) {
-	app.endBlocker = endBlocker
-}
-
 func (app *BaseApp) PushAnteHandler(ah sdk.AnteHandler) {
 	app.anteHandler = ah
-}
-
-func (app *BaseApp) SetTxDecoder(txDecoder sdk.TxDecoder) {
-	app.txDecoder = txDecoder
 }
