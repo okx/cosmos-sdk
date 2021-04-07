7429f1a8a4bd577c56a7cf9028d29f93a069143f
Green <gl12138@live.com>
2021-04-07 16:49:59 +0800

    prune

--- store/rootmulti/store.go
+++ store/rootmulti/store.go
@@ -306,24 +306,34 @@ func (rs *Store) Commit() types.CommitID {
 	version := previousHeight + 1
 	rs.lastCommitInfo = commitStores(version, rs.stores)
 
-	// Determine if pruneHeight height needs to be added to the list of heights to
-	// be pruned, where pruneHeight = (commitHeight - 1) - KeepRecent.
-	if int64(rs.pruningOpts.KeepRecent) < previousHeight {
-		pruneHeight := previousHeight - int64(rs.pruningOpts.KeepRecent)
-		// We consider this height to be pruned iff:
-		//
-		// - KeepEvery is zero as that means that all heights should be pruned.
-		// - KeepEvery % (height - KeepRecent) != 0 as that means the height is not
-		// a 'snapshot' height.
-		if rs.pruningOpts.KeepEvery == 0 || pruneHeight%int64(rs.pruningOpts.KeepEvery) != 0 {
-			rs.pruneHeights = append(rs.pruneHeights, pruneHeight)
+	indexHeight := tmtypes.GetStartBlockHeight() + 1
+	for ; indexHeight <= previousHeight; indexHeight++ {
+		if indexHeight % int64(rs.pruningOpts.KeepEvery) == 0{
+			rs.pruneHeights = append(rs.pruneHeights, indexHeight)
 		}
 	}
-
-	// batch prune if the current height is a pruning interval height
-	if rs.pruningOpts.Interval > 0 && version%int64(rs.pruningOpts.Interval) == 0 {
-		rs.pruneStores()
+	for index := previousHeight-int64(rs.pruningOpts.KeepRecent); index <= previousHeight;index++{
+		rs.pruneHeights = append(rs.pruneHeights, indexHeight)
 	}
+	rs.pruneStores()
+	// Determine if pruneHeight height needs to be added to the list of heights to
+	// be pruned, where pruneHeight = (commitHeight - 1) - KeepRecent.
+	//if int64(rs.pruningOpts.KeepRecent) < previousHeight {
+	//	pruneHeight := previousHeight - int64(rs.pruningOpts.KeepRecent)
+	//	// We consider this height to be pruned iff:
+	//	//
+	//	// - KeepEvery is zero as that means that all heights should be pruned.
+	//	// - KeepEvery % (height - KeepRecent) != 0 as that means the height is not
+	//	// a 'snapshot' height.
+	//	if rs.pruningOpts.KeepEvery == 0 || pruneHeight%int64(rs.pruningOpts.KeepEvery) != 0 {
+	//		rs.pruneHeights = append(rs.pruneHeights, pruneHeight)
+	//	}
+	//}
+
+	//// batch prune if the current height is a pruning interval height
+	//if rs.pruningOpts.Interval > 0 && version%int64(rs.pruningOpts.Interval) == 0 {
+	//	rs.pruneStores()
+	//}
 
 	flushMetadata(rs.db, version, rs.lastCommitInfo, rs.pruneHeights)
 
