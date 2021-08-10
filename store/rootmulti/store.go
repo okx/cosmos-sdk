9b7679fa7a2466a2fccd57bdb532cbdb3470e1e7
ylsGit <bzwdyls@163.com>
2021-08-10 17:49:28 +0800

    support prune app store

--- store/rootmulti/store.go
+++ store/rootmulti/store.go
@@ -89,6 +89,10 @@ func (rs *Store) GetStoreType() types.StoreType {
 	return types.StoreTypeMulti
 }
 
+func (rs *Store) GetStores() map[types.StoreKey]types.CommitKVStore {
+	return rs.stores
+}
+
 // Implements CommitMultiStore.
 func (rs *Store) MountStoreWithDB(key types.StoreKey, typ types.StoreType, db dbm.DB) {
 	if key == nil {
@@ -144,6 +148,10 @@ func (rs *Store) LoadVersionAndUpgrade(ver int64, upgrades *types.StoreUpgrades)
 func (rs *Store) LoadLatestVersion() error {
 	ver := getLatestVersion(rs.db)
 	return rs.loadVersion(ver, nil)
+}
+
+func (rs *Store) GetLatestVersion() int64 {
+	return getLatestVersion(rs.db)
 }
 
 // LoadVersion implements CommitMultiStore.
@@ -378,6 +386,10 @@ func (rs *Store) pruneStores() {
 	}
 
 	rs.pruneHeights = make([]int64, 0)
+}
+
+func (rs *Store) FlushPruneHeights(pruneHeights []int64, versions []int64) {
+	flushMetadata(rs.db, rs.lastCommitInfo.Version, rs.lastCommitInfo, pruneHeights, versions)
 }
 
 // Implements CacheWrapper/Store/CommitStore.
@@ -926,7 +938,7 @@ func (rs *Store) buildCommitInfo(version int64) commitInfo {
 
 func (src Store) Copy() *Store {
 	dst := &Store{
-		db: src.db,
+		db:           src.db,
 		pruningOpts:  src.pruningOpts,
 		storesParams: make(map[types.StoreKey]storeParams, len(src.storesParams)),
 		stores:       make(map[types.StoreKey]types.CommitKVStore, len(src.stores)),
@@ -941,7 +953,7 @@ func (src Store) Copy() *Store {
 	}
 
 	dst.lastCommitInfo = commitInfo{
-		Version: src.lastCommitInfo.Version,
+		Version:    src.lastCommitInfo.Version,
 		StoreInfos: make([]storeInfo, 0),
 	}
 
