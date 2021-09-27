4beb1a23329a229ad3c3a6220f45e9284d9f0d88
louisliu2048 <louisliu512@163.com>
2021-09-27 19:05:50 +0800

    recover raw condition

--- store/rootmulti/store.go
+++ store/rootmulti/store.go
@@ -358,8 +358,10 @@ func (rs *Store) Commit() types.CommitID {
 	}
 
 	// batch prune if the current height is a pruning interval height
-	if !iavltree.EnableOptPruing {
-		rs.pruneStores() // use pruning logic from iavl project
+	if rs.pruningOpts.Interval > 0 && version%int64(rs.pruningOpts.Interval) == 0 {
+		if !iavltree.EnableOptPruing {
+			rs.pruneStores() // use pruning logic from iavl project
+		}
 	}
 
 	rs.versions = append(rs.versions, version)
