0bb36d2528075775104fb920ccee74bd7dde4ac5
chunfengSun <516108736@qq.com>
2021-10-22 00:44:30 +0800

    Update baseapp_parallel.go

--- baseapp/baseapp_parallel.go
+++ baseapp/baseapp_parallel.go
@@ -11,7 +11,7 @@ import (
 func (app *BaseApp) PrepareParallelTxs(cb abci.AsyncCallBack, txs [][]byte) {
 	app.parallelTxManage.workgroup.Cb = cb
 	//app.parallelTxManage.isAsyncDeliverTx = true
-	app.parallelTxManage.ignoreAnteErr = append(app.parallelTxManage.ignoreAnteErr, true)
+	app.parallelTxManage.ignoreAnteErr = append(app.parallelTxManage.ignoreAnteErr, false)
 	app.parallelTxManage.isAsyncDeliverTx = true
 	evmIndex := uint32(0)
 	for k, v := range txs {
